46fe005d10fda25f06587c112992879e0922a97a
Ornella Ramos <ornella.ramos.velandia@gmail.com>
2019-03-27 15:04:59 -0500

    Adding urls file to profiles_api

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_frameworks.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+
+
+class HelloApiView(APIView):
+    """Test API View."""
+
+    def get(self, request, format=None):
+        """Returns a list of a ApiView features."""
+
+        an_apiview = [
+            'Uses HTTP methods as function (get, post, patch, put, delete).',
+            'It is similar to a traditional Django view.',
+            'Gives you the most control over your logic.',
+            'Is mapped manually to URLs.'
+
+        ]
+
+        return Response({'message': 'Hello', 'an_apiview': an_apiview})
+
