b26a103363693108cf8be4a510fd579f7d454230
Ornella Ramos <ornella.ramos.velandia@gmail.com>
2019-03-27 15:12:43 -0500

    Created our first APIView

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_frameworks.views import APIView
+from rest_framework.views import APIView
 from rest_framework.response import Response
 
 # Create your views here.
